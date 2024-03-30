cb22a56787df4993a0c1d294180368c079675ff6
Nndaaa404 <anandaarizkymaulana@gmail.com>
2024-03-30 16:06:49 +0700

    Belajar 10 (User Login & Middleware)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -41,7 +42,7 @@ Route::get('/posts', [PostController::class, 'index']);
 // halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-route::get('/categories', function() {
+route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
         'active' => 'categories',
@@ -66,7 +67,11 @@ route::get('/categories', function() {
 //     ]);
 // });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
